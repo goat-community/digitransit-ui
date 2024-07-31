b0d6d0c70f1d03f318c347ffc37b86d861fc3cb1
EPajares <elias.pajares@plan4better.de>
2024-07-31 13:39:34 +0200

    fix: Update email address for feedback in KL.Navi configuration

--- app/configurations/config.klnavi.js
+++ app/configurations/config.klnavi.js
@@ -465,7 +465,7 @@ export default configMerger(walttiConfig, {
                     'Sie betreiben ein Mobilitätsangebot in Kaiserslautern und haben es noch nicht in KL.Navi gefunden? Dann freuen wir uns über Ihre Nachricht. Statistische und Echtzeit-Informationen, die als offene Daten vorliegen, binden wir gerne ein.',
                     'Sie nutzen ein Mobilitätsangebot in Brandenburg (z.B. einen Bürgerbus oder ein Fahrrad- oder Carsharing-Angebot) und finden es nicht in KL.Navi? Dann freuen wir uns über Ihre Hinweise und gucken dann, ob wir es in KL.Navi einbinden können.',
                     'Sie möchten zum Mitmacher werden und wünschen mehr Informationen?',
-                    'Schreiben Sie uns eine E-mail an lotse@kaiserslautern.de',
+                    'Schreiben Sie uns eine E-mail an <a href=mailto:lotse@kaiserslautern.de>lotse@kaiserslautern.de</a>.',
                 ]
             },
             {
